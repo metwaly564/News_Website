449684b8132f34b9b6e359025b4f15b10b887172
Mohamed Metwaly <84545984+metwaly564@users.noreply.github.com>
2023-03-02 19:50:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function remove()
+{
+  aside = document.getElementsByClassName("aside")[0];
+  aside.style.display='none';
+}
+
+aside=document.getElementsByClassName("aside")[0];
+function show()
+{
+  // aside.classList.toggle("show22");
+  aside.style.display='flex';
+    
+}
